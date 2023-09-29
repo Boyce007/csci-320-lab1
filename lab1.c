5bc9a33258dd8d90bc043cfb3d2b6b9257e45643
Boyce007 <diboyce21@students.desu.edu>
2023-09-29 21:51:26 +0000

    made updates to substring

---
+++ lab1.c
@@ -0,0 +1,34 @@
+/*
+ * Implement your solution in thi file
+ *
+ */
+#include <string.h>
+char* readString(char* filename) {
+
+}
+char* substring(char* input, int start, int end ) {
+    int c = 0;
+    char output; 
+
+    char* outputptr;
+    outputptr  = malloc((start- end)*sizeof(char));
+    strcat(output,outputptr);
+    while (c<start) {
+    
+    }
+
+    
+}
+char* explode(char* input) {
+    char* result ='\0';
+    #define int length = strlen(*input);
+    char* resultptr = (unsigned char*) malloc(((length(length+1))/2) +1 * sizeof(char));
+
+
+
+    // void *memcpy(void *dest, void *src, size_t count);
+    // char *strcat(char *dest, const char *src)
+
+}
+
+
